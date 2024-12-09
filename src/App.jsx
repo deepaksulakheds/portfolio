fddc13730d80ae230d468dbf9c7be7325b42015d
Deepak Sulakhe <deepak@zeliot.in>
2024-12-09 13:10:43 +0530

    add: Added multiple snackbar and fixes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Body from "./Components/Body/Body";
-import { SnackbarProvider } from "./Components/SharedSnackbar/SharedSnackbar";
+// import { SnackbarProvider } from "./Components/SharedSnackbar/SharedSnackbar";
 import { AttachmentProvider } from "./Components/MailDialog/attachmentContext";
 import { ApolloProvider } from "@apollo/client";
-
 import { client } from "./clients.js";
+import { NotistackSnackbarProvider } from "./Components/SharedSnackbar/SharedSnackbar1";
 
 function App() {
-  // console.clear();
-
   return (
     <>
       <ApolloProvider client={client}>
         <AttachmentProvider>
-          <SnackbarProvider>
+          <NotistackSnackbarProvider>
             <Header />
             <Body />
-          </SnackbarProvider>
+          </NotistackSnackbarProvider>
         </AttachmentProvider>
       </ApolloProvider>
     </>
