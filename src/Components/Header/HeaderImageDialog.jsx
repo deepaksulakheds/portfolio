54f9a378a2b3c0f9870dfe80de914b563682fdb4
Deepak Sulakhe <deepak@zeliot.in>
2025-03-14 17:00:07 +0530

    add: added multiple images in header image

--- src/Components/Header/HeaderImageDialog.jsx
+++ src/Components/Header/HeaderImageDialog.jsx
@@ -1,32 +1,86 @@
-import { Dialog } from "@mui/material";
+import { Dialog, Grid } from "@mui/material";
 import "./header.css";
+import { useEffect, useState } from "react";
+
+const imagesList = ["./deepak.jpg", `deepak-1.jpg`, `deepak-2.jpg`];
 
 export default function HeaderImageDialog({ imageDialogVisible, onClose }) {
+  const [selectedImage, setSelectedImage] = useState(imagesList[0]);
+
+  useEffect(() => {
+    if (imageDialogVisible) {
+      // Uncomment for random pics
+      //   setSelectedImage(
+      //     imagesList[Math.floor(Math.random() * imagesList.length)]
+      //   );
+
+      return () => {
+        setTimeout(() => {
+          setSelectedImage(imagesList[0]);
+        }, 200);
+      };
+    }
+  }, [imageDialogVisible]);
+
+  const handleImageClick = (index) => {
+    setSelectedImage(imagesList[index]);
+  };
+
   return (
     <Dialog
       open={imageDialogVisible}
       onClose={onClose}
       fullWidth
-      sx={{ backdropFilter: "blur(10px)", boxShadow: "none" }}
+      sx={{ backdropFilter: "blur(12px)", boxShadow: "none" }}
       PaperProps={{
         sx: {
           boxShadow: "none",
           background: "none",
           height: "auto",
           width: "100%",
-          overflow: "hidden",
+          // overflow: "hidden",
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
         },
       }}
     >
       <img
-        src="./deepak.jpg"
+        src={selectedImage}
         style={{
           padding: "30px",
           // height: "85%",
           borderRadius: "50%",
-          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "rgba(255, 255, 255, 0.2)",
         }}
       />
+      <Grid
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <Grid
+          sx={{
+            display: "flex",
+            gap: "20px",
+            padding: "5px",
+            borderRadius: "13px",
+            // paddingLeft: "50px",
+            // paddingRight: "50px",
+            // border: "1px solid rgba(255, 255, 255, 0.5)",
+          }}
+        >
+          {imagesList.map((image, index) => (
+            <img
+              onClick={() => handleImageClick(index)}
+              key={index}
+              src={image}
+              className="imageList"
+            />
+          ))}
+        </Grid>
+      </Grid>
     </Dialog>
   );
 }
