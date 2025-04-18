ac738e310d8286ec0227d0967220539728e1d3a0
Deepak Sulakhe <deepak@zeliot.in>
2025-04-18 12:17:14 +0530

    add: added selected filter

--- src/Components/Notes/NotesComponent.jsx
+++ src/Components/Notes/NotesComponent.jsx
@@ -1,5 +1,6 @@
 import {
   Checkbox,
+  Chip,
   CircularProgress,
   Grid,
   IconButton,
@@ -12,6 +13,7 @@ import { DELETE_MULTIPLE_NOTES, GET_NOTES } from "../queries";
 import { AddBox, DisabledByDefault, Delete } from "@mui/icons-material";
 import { withNotistackSnackbar } from "../SharedSnackbar/SharedSnackbar1";
 import NotesDialog from "./NotesDialog.jsx";
+let displayedAll = false;
 
 function NotesComponent({ notistackSnackbar }) {
   const [getNotes, { data, loading, error }] = useLazyQuery(GET_NOTES, {
@@ -24,10 +26,17 @@ function NotesComponent({ notistackSnackbar }) {
   const [deleteMultipleNotes] = useMutation(DELETE_MULTIPLE_NOTES);
   const [noteAnchorEl, setNoteAnchorEl] = useState(null);
   const [checkedNotes, setCheckedNotes] = useState([]);
+  const [notesToDisplay, setNotesToDisplay] = useState([]);
 
   const fetchNotes = async () => {
     try {
-      await getNotes();
+      const resp = await getNotes();
+      // console.log("resp", resp.data.getAllNotes.response);
+      if (resp?.data?.getAllNotes?.response?.length > 0) {
+        setNotesToDisplay(resp.data.getAllNotes.response);
+      } else {
+        setNotesToDisplay([]);
+      }
     } catch (err) {
       // console.log("err", err);
       notistackSnackbar.showSnackbar(err.message, "error");
@@ -47,13 +56,13 @@ function NotesComponent({ notistackSnackbar }) {
             delResp.data.deleteMultipleNotes.message,
             "success"
           );
+          fetchNotes();
         } else {
           notistackSnackbar.showSnackbar(
             delResp.data.deleteMultipleNotes.message,
             "error"
           );
         }
-        fetchNotes();
         setCheckedNotes([]);
       } else {
         notistackSnackbar.showSnackbar("Please select note.", "error");
@@ -77,8 +86,26 @@ function NotesComponent({ notistackSnackbar }) {
   const handleClearSelection = () => {
     if (checkedNotes.length > 0) {
       setCheckedNotes([]);
+      setNotesToDisplay(data.getAllNotes.response);
+      displayedAll = false;
     }
     notistackSnackbar.showSnackbar("Selection cleared.", "info");
+  };
+
+  const toggleSelected = () => {
+    const isAllSelected = notesToDisplay.every((note) =>
+      checkedNotes.includes(note.id)
+    );
+    // console.log("isAllSelected", isAllSelected);
+    if (isAllSelected) {
+      setNotesToDisplay(data.getAllNotes.response);
+      displayedAll = false;
+    } else {
+      setNotesToDisplay(
+        checkedNotes.map((id) => notesToDisplay.find((note) => note.id === id))
+      );
+      displayedAll = true;
+    }
   };
 
   useEffect(() => {
@@ -86,105 +113,127 @@ function NotesComponent({ notistackSnackbar }) {
   }, []);
 
   return (
-    <Grid
-      sx={{ display: "flex", gap: "10px", justifyContent: "space-between" }}
-    >
-      <Grid sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-        {loading ? (
-          <CircularProgress color="white" />
-        ) : data && data?.getAllNotes?.response.length === 0 ? (
-          <Typography sx={{ fontWeight: "500" }}>No Data Found.</Typography>
-        ) : (
-          data?.getAllNotes?.response.map((note, index) => (
-            <Grid
-              key={note.id}
-              sx={{
-                border: "0.5px solid rgba(255, 255, 255, 0.24)",
-                display: "flex",
-                justifyContent: "space-between",
-                padding: "15px",
-                borderRadius: "10px",
-              }}
-              width={370}
-            >
-              <Typography
+    <Grid>
+      <Chip
+        sx={{
+          backgroundColor: displayedAll ? "rgba(170, 137, 242, 1)" : "white",
+          fontWeight: "bold",
+          marginBottom: "12px",
+          transition: "all ease-in-out .2s",
+          ":hover": {
+            backgroundColor: "rgba(170, 137, 242, 1)",
+          },
+        }}
+        disabled={checkedNotes.length === 0}
+        label={`${checkedNotes.length} selected`}
+        onClick={toggleSelected}
+      />
+      <Grid
+        sx={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "space-between",
+          transition: "all ease-in-out .2s",
+        }}
+      >
+        <Grid sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          {loading ? (
+            <CircularProgress color="white" />
+          ) : notesToDisplay === 0 ? (
+            <Typography sx={{ fontWeight: "500" }}>No Data Found.</Typography>
+          ) : (
+            notesToDisplay.map((note, index) => (
+              <Grid
+                key={note.id}
                 sx={{
-                  fontWeight: "500",
-                  wordBreak: "break-word",
+                  border: "0.5px solid rgba(255, 255, 255, 0.24)",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  padding: "15px",
+                  borderRadius: "10px",
                 }}
+                width={370}
               >
-                {note.note}
-              </Typography>
-              <IconButton
-                target="blank"
-                sx={{
-                  alignSelf: "flex-start",
-                  padding: "0.2rem",
-                  color: "white",
-                  transition: "all ease-in-out 0.15s",
-                  "&:hover": {
-                    boxShadow: "inset 0px 0px 22px 0px rgba(170, 137, 242, 1)",
-                  },
-                }}
-              >
-                {/* <DeleteOutline onClick={(e) => removeNote(note.id)} /> */}
-                <Checkbox
+                <Typography
                   sx={{
-                    color: "rgba(255, 255, 255, 0.6)",
-                    margin: 0,
-                    padding: 0,
-                    "&.Mui-checked": {
-                      color: "rgba(170, 137, 242, 1)",
+                    fontWeight: "500",
+                    wordBreak: "break-word",
+                  }}
+                >
+                  {note.note}
+                </Typography>
+                <IconButton
+                  target="blank"
+                  sx={{
+                    alignSelf: "flex-start",
+                    padding: "0.2rem",
+                    color: "white",
+                    transition: "all ease-in-out 0.15s",
+                    "&:hover": {
+                      boxShadow:
+                        "inset 0px 0px 22px 0px rgba(170, 137, 242, 1)",
                     },
                   }}
-                  checked={checkedNotes.includes(note.id)}
-                  onClick={(e) => handleCheck(note.id)}
-                />
-              </IconButton>
-            </Grid>
-          ))
-        )}
+                >
+                  {/* <DeleteOutline onClick={(e) => removeNote(note.id)} /> */}
+                  <Checkbox
+                    sx={{
+                      color: "rgba(255, 255, 255, 0.6)",
+                      margin: 0,
+                      padding: 0,
+                      "&.Mui-checked": {
+                        color: "rgba(170, 137, 242, 1)",
+                      },
+                    }}
+                    checked={checkedNotes.includes(note.id)}
+                    onClick={(e) => handleCheck(note.id)}
+                  />
+                </IconButton>
+              </Grid>
+            ))
+          )}
+        </Grid>
+        <Grid sx={{ display: "flex", gap: "25px", flexDirection: "column" }}>
+          <AddBox
+            sx={{
+              cursor: "pointer",
+              color: "white",
+              "&:hover": {
+                color: "rgba(170, 137, 242, 1)",
+              },
+            }}
+            onClick={(e) =>
+              setNoteAnchorEl((prev) => (prev ? null : e.currentTarget))
+            }
+          />
+          <Delete
+            sx={{
+              cursor: "pointer",
+              color: "white",
+              "&:hover": {
+                color: "rgba(170, 137, 242, 1)",
+              },
+            }}
+            onClick={handleMultipleDelete}
+          />
+          <DisabledByDefault
+            sx={{
+              borderRadius: "50%",
+              cursor: "pointer",
+              color: "white",
+              "&:hover": {
+                color: "rgba(170, 137, 242, 1)",
+              },
+            }}
+            onClick={handleClearSelection}
+          />
+        </Grid>
+        <NotesDialog
+          noteAnchorEl={noteAnchorEl}
+          onClose={() => setNoteAnchorEl(null)}
+          fetchNotes={fetchNotes}
+        />
       </Grid>
-      <Grid sx={{ display: "flex", gap: "25px", flexDirection: "column" }}>
-        <AddBox
-          sx={{
-            cursor: "pointer",
-            color: "white",
-            "&:hover": {
-              color: "rgba(170, 137, 242, 1)",
-            },
-          }}
-          onClick={(e) =>
-            setNoteAnchorEl((prev) => (prev ? null : e.currentTarget))
-          }
-        />
-        <Delete
-          sx={{
-            cursor: "pointer",
-            color: "white",
-            "&:hover": {
-              color: "rgba(170, 137, 242, 1)",
-            },
-          }}
-          onClick={handleMultipleDelete}
-        />
-        <DisabledByDefault
-          sx={{
-            borderRadius: "50%",
-            cursor: "pointer",
-            color: "white",
-            "&:hover": {
-              color: "rgba(170, 137, 242, 1)",
-            },
-          }}
-          onClick={handleClearSelection}
-        />
-      </Grid>
-      <NotesDialog
-        noteAnchorEl={noteAnchorEl}
-        onClose={() => setNoteAnchorEl(null)}
-        fetchNotes={fetchNotes}
-      />
     </Grid>
   );
 }
