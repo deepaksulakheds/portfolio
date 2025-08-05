b2514255bdbbc75026bf649ae43401506e7b7582
Deepak Sulakhe <deepak@zeliot.in>
2025-08-05 17:29:43 +0530

    MUI depricated props updates

--- src/Components/Notes/NotesDialog.jsx
+++ src/Components/Notes/NotesDialog.jsx
@@ -150,8 +150,18 @@ function NotesDialog({
         <TextField
           autoFocus
           multiline
-          InputLabelProps={{ style: { color: themeContext.dullOppositeTheme } }}
-          InputProps={{ style: { color: themeContext.oppositeTheme } }}
+          slotProps={{
+            input: {
+              style: {
+                color: themeContext.oppositeTheme,
+              },
+            },
+            inputLabel: {
+              style: {
+                color: themeContext.dullOppositeTheme,
+              },
+            },
+          }}
           sx={{
             "& .MuiInput-underline:before": {
               borderBottom: "1px solid",
